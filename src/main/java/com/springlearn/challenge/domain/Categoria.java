d8f669636c0aa53ee8912c7d5057534f0cb59423
LeandroSAlmeida <contact.dev.leandro@gmail.com>
2023-12-21 21:34:24 -0300

    Class Categoria

---
+++ src/main/java/com/springlearn/challenge/domain/Categoria.java
@@ -0,0 +1,33 @@
+package com.springlearn.challenge.domain;
+
+import java.util.Objects;
+
+public class Categoria {
+    private Integer id;
+    private String descricao;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
